f6147588713c1c7a9ffc66ec04e63bb341f56493
mengwenwang <mengwenwang@users.noreply.github.com>
2017-12-27 21:50:33 -0500

    Add files via upload

---
+++ jobs.php
@@ -0,0 +1,14 @@
+<?php include '../partials/head_en.php' ?>
+    <input type="text" value="jobs" id="activeIdentifier" hidden>   
+    <section class="jobsSection">
+        <div class="jobsTopContainer">
+            <div class="topText">
+                <h1>Jobs</h1>
+            </div>
+        </div>
+        <div class="jobsBox">
+            <a href="../jobs.docx" download>CIPTC Public School Teaching Jobs.docx</a>
+        </div>
+    </section>
+    
+<?php include '../partials/footer_en.php' ?>
